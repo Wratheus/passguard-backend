254e672844cbf6126ebe95e4b0492e0f5911840f
Aleksandr P. <alex-pavlenko-net-2013@yandex.ru>
2024-03-27 00:31:50 +0300

    initial commit

---
+++ lib/src/main/java/com/passguard/springboot/model/app/Build.java
@@ -0,0 +1,3 @@
+final class Build {
+
+}
---
+++ lib/src/test/java/com/passguard/demo/PassguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passguard.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
